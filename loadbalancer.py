3a6958d65e62e39482dbdfa5e0e9d890c9e19d4d
suman mandal <suman.buie.cpp@gmail.com>
2026-01-08 22:54:54 +0530

    added lock while updating the cnt as multithreading can wrongly messup the value & make duplex forword

--- loadbalancer.py
+++ loadbalancer.py
@@ -1,32 +1,44 @@
 from concurrent.futures import ThreadPoolExecutor
 import socket
+import threading
+
+lock = threading.Lock()
 
 SERVER_LIST = [8070, 8077, 8088]
+cnt = 0
 
 
-def forward_request_to_server(client_socket, cnt):
+def pipe(src, dst):
     try:
-        req = client_socket.recv(2048)
+        while True:
+            data = src.recv(4096)
+            if not data:
+                break
+            dst.sendall(data)
+    finally:
+        src.close()
+        dst.close()
 
-        port = SERVER_LIST[cnt % len(SERVER_LIST)]
+
+def forward_request_to_server(client_socket):
+    try:
+        global cnt
+
+        with lock:
+            port = SERVER_LIST[cnt % len(SERVER_LIST)]
+            cnt+=1
+        
         print(f"Forwarding request to backend localhost:{port}")
 
         backend_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         backend_sock.connect(("127.0.0.1", port))
+        threading.Thread(target=pipe, args=(client_socket, backend_sock), daemon=True).start()
+        threading.Thread(target=pipe, args=(backend_sock, client_socket), daemon=True).start()
 
-        backend_sock.sendall(req)
-        resp = backend_sock.recv(2048)
-
-        backend_sock.close()
-
-        client_socket.sendall(resp)
         print("Response sent back to client")
 
     except Exception as e:
         print("Error:", e)
-
-    finally:
-        client_socket.close()
 
 if __name__ == "__main__":
     pool = ThreadPoolExecutor(max_workers=4)
@@ -37,9 +49,7 @@ if __name__ == "__main__":
 
     print("Load balancer listening on port 8081...")
 
-    cnt = 0
     while True:
         client_socket, addr = lb_sock.accept()
         print(f"Request coming from {addr[0]}:{addr[1]}")
-        pool.submit(forward_request_to_server, client_socket, cnt)
-        cnt += 1
+        pool.submit(forward_request_to_server, client_socket)
